21df7c4b9c1488059a4580cd06dbb4fa8c432ad3
lawalhayatu <hayatubidemi@gmail.com>
2026-03-18 20:12:44 +0100

    created models files

---
+++ models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+class Patient:
+    def __init__(self, name):
+        self.name = name
+        self.time_added = datetime.now()
+
+    def get_details(self):
+        return f"{self.name} (Added at {self.time_added.strftime('%H:%M:%S')})"
+
+
+class ClinicQueue:
+    def __init__(self):
+        self.queue = []
+        self.served = 0
+
+    def add_patient(self, patient):
+        self.queue.append(patient)
+
+    def serve_patient(self):
+        if self.queue:
+            self.served += 1
+            return self.queue.pop(0)  # FIFO
+        return None
+
+    def get_all_patients(self):
+        return self.queue
+
+    def total_served(self):
+        return self.served
